91846a6128a967639ca5e2fba06b63347e0d5a10
zhelezniakmatviy <zhelezniakmatvey@gmail.com>
2017-03-13 00:17:54 +0200

    Add new projext with div background video

---
+++ assets/js/custom.js
@@ -0,0 +1,66 @@
+﻿(function($) {
+    "use strict";
+    var mainApp = {
+        scroll_fun: function() {
+
+
+            $(function() {
+                $('.move-me a').bind('click', function(event) {
+                    var $anchor = $(this);
+                    $('html, body').stop().animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    }, 1000, 'easeInOutQuad');
+                    event.preventDefault();
+                });
+            });
+
+
+
+        },
+
+        gallery_fun: function() {
+
+            $("a.preview").prettyPhoto({
+                social_tools: false
+            });
+
+
+
+
+
+            $('#port-folio').mixitup({
+                targetSelector: '.portfolio-item',
+                filterSelector: '.filter',
+
+
+            });
+        },
+        vedio_fun: function() {
+            $(function() {
+                $(".player").mb_YTPlayer();
+            });
+        },
+
+        nice_scroll_fun: function() {
+            $("html").niceScroll();
+        },
+        custom_fun: function() {
+
+
+
+
+
+        },
+
+    }
+
+
+    $(document).ready(function() {
+        mainApp.scroll_fun();
+        mainApp.gallery_fun();
+        mainApp.vedio_fun();
+        mainApp.nice_scroll_fun();
+        mainApp.custom_fun();
+
+    });
+}(jQuery));
